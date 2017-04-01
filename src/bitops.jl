8b974e43c2c06dd83bc9a9eb0fc1c55305ee8508
Oliver Schulz <oschulz@mpp.mpg.de>
2017-04-01 12:57:18 +0200

    Cast bit numbers to appropriate unsigned values for higher performance
    Bit shifts perform separate handling for positive and negative shift
    values, internally, yielding less tight code if shift values are signed.
    
    Also avoids some inlining issues.

--- src/bitops.jl
+++ src/bitops.jl
@@ -37,58 +37,66 @@ function msbget end
 export msbget
 
 
+@inline fbc{T}(::Type{T}, x) = x%unsigned(T)
+@inline fbc{T,U}(::Type{T}, bits::UnitRange{U}) = fbc(T, bits.start):fbc(T, bits.stop)
+
+
 @inline bsizeof(x) = sizeof(x) << 3
 
 
-@inline bmask{T <: Integer}(::Type{T}, bit::BitCount) = one(T) << bit
+@inline bmask{T <: Integer}(::Type{T}, bit::BitCount) = one(T) << fbc(T, bit)
 
 @inline bmask{T <: Integer, U <: BitCount}(::Type{T}, bits::UnitRange{U}) = begin
-  #@assert bits.stop >= bits.start "Bitmask range of bits can't be reverse"
-  ((one(T) << (bits.stop - bits.start + 1)) - one(T)) << bits.start
+    fbits = fbc(T, bits)
+    #@assert fbits.stop >= fbits.start "Bitmask range of fbits can't be reverse"
+    ((one(T) << (fbits.stop - fbits.start + 1)) - one(T)) << fbits.start
 end
 
 
 @inline lsbmask{T <: Integer}(::Type{T}) = one(T)
 
-@inline msbmask{T <: Integer}(::Type{T}) = one(T) << (bsizeof(T) - 1)
+@inline msbmask{T <: Integer}(::Type{T}) = one(T) << fbc(T, bsizeof(T) - 1)
 
 
-@inline lsbmask{T <: Integer}(::Type{T}, nbits::BitCount) = ~(~zero(T) << nbits)
+@inline lsbmask{T <: Integer}(::Type{T}, nbits::BitCount) = ~(~zero(T) << fbc(T, nbits))
 
-@inline msbmask{T <: Integer}(::Type{T}, nbits::BitCount) = ~(~zero(T) >>> nbits)
-
-
-@inline bget(x::Integer, bit::BitCount) = x & bmask(typeof(x), bit) != zero(typeof(x))
-
-@inline bset(x::Integer, bit::BitCount, y::Bool) = y ? bset(x, bit) : bclear(x, bit)
-@inline bset(x::Integer, bit::BitCount) = x | bmask(typeof(x), bit)
-
-@inline bclear(x::Integer, bit::BitCount) = x & ~bmask(typeof(x), bit)
+@inline msbmask{T <: Integer}(::Type{T}, nbits::BitCount) = ~(~zero(T) >>> fbc(T, nbits))
 
 
-@inline bget{U <: BitCount}(x::Integer, bits::UnitRange{U}) =
-    (x & bmask(typeof(x), bits)) >>> bits.start
+@inline bget{T <: Integer}(x::T, bit::BitCount) = x & bmask(typeof(x), fbc(T, bit)) != zero(typeof(x))
 
-@inline bset{U <: BitCount}(x::Integer, bits::UnitRange{U}, y::Integer) = begin
-    local bm = bmask(typeof(x), bits)
-    (x & ~bm) | ((convert(typeof(x), y) << bits.start) & bm)
+@inline bset{T <: Integer}(x::T, bit::BitCount, y::Bool) = y ? bset(x, fbc(T, bit)) : bclear(x, fbc(T, bit))
+@inline bset{T <: Integer}(x::T, bit::BitCount) = x | bmask(typeof(x), fbc(T, bit))
+
+@inline bclear{T <: Integer}(x::T, bit::BitCount) = x & ~bmask(typeof(x), fbc(T, bit))
+
+
+@inline bget{T <: Integer, U <: BitCount}(x::T, bits::UnitRange{U}) = begin
+    fbits = fbc(T, bits)
+    (x & bmask(typeof(x), fbits)) >>> fbits.start
+end
+
+@inline bset{T <: Integer, U <: BitCount}(x::T, bits::UnitRange{U}, y::Integer) = begin
+    fbits = fbc(T, bits)
+    local bm = bmask(typeof(x), fbits)
+    (x & ~bm) | ((convert(typeof(x), y) << fbits.start) & bm)
 end
 
 
-@inline bflip(x::Integer, bit::BitCount) = xor(x, bmask(typeof(x), bit))
+@inline bflip{T <: Integer}(x::T, bit::BitCount) = xor(x, bmask(typeof(x), fbc(T, bit)))
 
-@inline bflip{U <: BitCount}(x::Integer, bits::UnitRange{U}) = xor(x, bmask(typeof(x), bits))
+@inline bflip{T <: Integer, U <: BitCount}(x::T, bits::UnitRange{U}) = xor(x, bmask(typeof(x), fbc(T, bits)))
 
 
-@inline lsbget(x::Integer) =
+@inline lsbget{T <: Integer}(x::T) =
     x & lsbmask(typeof(x)) != zero(typeof(x))
 
-@inline msbget(x::Integer) =
+@inline msbget{T <: Integer}(x::T) =
     x & msbmask(typeof(x)) != zero(typeof(x))
 
 
-@inline lsbget(x::Integer, nbits::BitCount) =
-    x & lsbmask(typeof(x), nbits)
+@inline lsbget{T <: Integer}(x::T, nbits::BitCount) =
+    x & lsbmask(typeof(x), fbc(T, nbits))
 
-@inline msbget(x::Integer, nbits::BitCount) =
-    x >>> (bsizeof(x) - nbits)
+@inline msbget{T <: Integer}(x::T, nbits::BitCount) =
+    x >>> (bsizeof(x) - fbc(T, nbits))
