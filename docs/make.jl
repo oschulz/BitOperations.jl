1cd020a8c13915161c35372e87df52f3ac25a136
Oliver Schulz <oschulz@mpp.mpg.de>
2021-02-02 15:08:41 +0100

    Use GitHub actions instead of Travis, update to Documenter.jl v0.26

--- docs/make.jl
+++ docs/make.jl
@@ -14,17 +14,18 @@ makedocs(
         prettyurls = !("local" in ARGS),
         canonical = "https://oschulz.github.io/BitOperations.jl/stable/"
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "API" => "api.md",
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    linkcheck = ("linkcheck" in ARGS),
+    linkcheck = !("nonstrict" in ARGS),
     strict = !("nonstrict" in ARGS),
 )
 
 deploydocs(
     repo = "github.com/oschulz/BitOperations.jl.git",
-    forcepush = true
+    forcepush = true,
+    push_preview = true,
 )
